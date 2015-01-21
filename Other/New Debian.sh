5b73f1b793dc4f476323c33fd2b73a956e6fe4dc
Jack Ellis <Jack@System-X.local>
2015-01-21 17:07:46 +0000

    Added a debian shell script

---
+++ Other/New Debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install chromium-browser
+
+sudo apt-get remove vim-tiny
+
+sudo apt-get install vim
+
+sudo apt-get install git
+
+git config --global user.name "Jack"
+
+git config --global user.email "jackellis1504@me.com"
