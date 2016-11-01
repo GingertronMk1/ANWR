68caccfc2550731e0fef21fda6974c4afd55b72c
Jack Ellis <jackellis1504@me.com>
2016-11-01 11:01:34 +0000

    newSystem was, for some reason, creating a symlink loop in my vimcolors folder. I made it stop, everything's alright again

--- NewSystem.sh
+++ NewSystem.sh
@@ -8,14 +8,14 @@ cd ~
 
 for dotFile in .aliases .bash_profile .bashrc .exports .functions .gitconfig .inputrc .prompt .vimrc .xsessionrc
 do
-    if [ -e $dotFile ]; then
-        rm -f $dotFile
-    fi
-    ln -s $scriptDir/$dotFile $dotFile
+    [ -e $dotFile ] && (rm -f $dotFile; echo "Deleted existing copy of $dotFile") # If the dotfile already exists, delete it
+    ln -s $scriptDir/$dotFile $dotFile  # Symlink to this directory's copy
+    echo "Linked $dotFile to copy in ANWR"
 done
 
-mkdir -p .vim/          # Make vim look right for me
+[ ! -e .vim ] && (mkdir -p .vim; echo "Creating .vim folder")        # If the .vim folder doesn't exist, create it
+[ -e .vim/colors ] && (rm -rf .vim/colors; echo "Deleting .vim/colors folder")  # If the file ~/.vim/colors exists, delete it
 ln -s $scriptDir/vimcolors .vim/colors
-echo "Colour schemes added!"
+echo "Linked .vim/colors to color folder in ANWR"
 
 touch .hushlogin        # Create a .hushlogin file so the system shuts the fuck up when I log in
