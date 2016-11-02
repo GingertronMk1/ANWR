af9390150714583d4d3eb3e5b59a02b871d0c7f0
Jack Ellis <jackellis1504@me.com>
2016-11-02 20:18:34 +0000

    NewSystem.sh changes

--- NewSystem.sh
+++ NewSystem.sh
@@ -14,17 +14,17 @@ cd ~
 
 for file in $dotFiles
 do
-    [ -e $file ] && (rm $file; echo "Deleting existing copy of $file")
-    ln -s $dotDir/$file $file  # Symlink to this directory's copy
+    [ -e $file ] && (rm $file; echo "Deleting existing copy of $file in ~")
+    ln -s $dotDir/$file $file  # Create a symlink to this directory's copy in ~
 done
 
 [ ! -e .vim ] && (mkdir -p .vim; echo "Creating .vim folder")        # If the .vim folder doesn't exist, create it
-[ -e .vim/colors ] && (rm -rf .vim/colors; echo "Deleting .vim/colors folder")  # If the file/folder ~/.vim/colors exists, delete it
+[ -e .vim/colors ] && (rm -rf .vim/colors; echo "Deleting .vim/colors folder")  # If the file/folder .vim/colors exists, delete it
 ln -s $scriptDir/vimcolors .vim/colors                      # Symlink to the colors directory in this folder
 echo "Linked .vim/colors to color folder in ANWR"           # Tell me what it's done
 
 touch .hushlogin        # Create a .hushlogin file so the system shuts the fuck up when I log in
 
-cd .
-
 . .bashrc
+
+cd -
