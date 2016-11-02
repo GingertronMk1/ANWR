885d1ad04821e1bcd3ede4b6e5212c8976e6f184
Jack Ellis <jackellis1504@me.com>
2016-11-02 14:21:31 +0000

    Aliases update to make working with a Pi easier

--- NewSystem.sh
+++ NewSystem.sh
@@ -15,15 +15,16 @@ cd ~
 for file in $dotFiles
 do
     [ -e $file ] && (rm $file; echo "Deleting existing copy of $file")
-    #echo $dotDir/$file
     ln -s $dotDir/$file $file  # Symlink to this directory's copy
-    cd .
-    . $file
 done
 
 [ ! -e .vim ] && (mkdir -p .vim; echo "Creating .vim folder")        # If the .vim folder doesn't exist, create it
-[ -e .vim/colors ] && (rm -rf .vim/colors; echo "Deleting .vim/colors folder")  # If the file ~/.vim/colors exists, delete it
-ln -s $scriptDir/vimcolors .vim/colors
-echo "Linked .vim/colors to color folder in ANWR"
+[ -e .vim/colors ] && (rm -rf .vim/colors; echo "Deleting .vim/colors folder")  # If the file/folder ~/.vim/colors exists, delete it
+ln -s $scriptDir/vimcolors .vim/colors                      # Symlink to the colors directory in this folder
+echo "Linked .vim/colors to color folder in ANWR"           # Tell me what it's done
 
 touch .hushlogin        # Create a .hushlogin file so the system shuts the fuck up when I log in
+
+cd .
+
+. .bashrc
