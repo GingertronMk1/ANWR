3cfb048c17ae4e8edfe3bbc4781d05d2f33c49c7
Jack Ellis <jackellis1504@me.com>
2016-11-03 20:22:55 +0000

    ConvertAndTidy updates

--- Scripts/ConvertAndTidy.sh
+++ Scripts/ConvertAndTidy.sh
@@ -4,15 +4,12 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 musicPath=/Users/Jack/Desktop/Scratch
-#files=$(find ~/Music/iTunes/iTunes\ Media/Music -name *.m4a)
 files=$musicPath/*                           # Where the file things are (that you want copying)
 
 convert() {
     if [ "${1: -4}" == ".m4a" ]; then
         local m4aFile=$1
-        # Converting to MP3
         mp3File=$(echo $m4aFile | sed -e 's/m4a/mp3/')       # Make a filename for the mp3 version
-        # echo $mp3File
         echo "Converting $m4aFile to $mp3File"
         avconv -v quiet -i $m4aFile -ab 320k -ac 2 -ar 44100 $mp3File   # Convert from m4a to 320kbps mp3
         echo "Conversion successful!"
@@ -23,14 +20,15 @@ convert() {
 tidy(){
     if [ "${1: -4}" == ".mp3" ]; then
         local mp3File=$1
-        echo "Tidying $(basename $mp3File)"
+        mp3Base=$(basename $mp3File)
+        echo "Tidying $mp3Base"
         albumQuery="    album           : "             # Queries for grepping the output of avprobe
         albumArtistQuery="    album_artist    : "
 
         # This bit takes the metadata outputted by avprobe and filters it to album and album artist, respectively
         # The two "sed"s remove the bit of the line that's not the bit I want and replace : with _, as folders and : don't mix
-        album=$(echo $(echo $(avprobe $mp3File 2>&1 | grep $albumQuery) | sed -e "s/$albumQuery//") | sed -e "s/:/_/")
-        albumArtist=$(echo $(echo $(avprobe $mp3File 2>&1 | grep $albumArtistQuery) | sed -e "s/$albumArtistQuery//") | sed -e "s/:/_/")
+        album=$(echo $(echo $(avprobe $mp3File 2>&1 | grep $albumQuery) | sed -e "s/$albumQuery//") | sed -e "s/[:\/]/_/")
+        albumArtist=$(echo $(echo $(avprobe $mp3File 2>&1 | grep $albumArtistQuery) | sed -e "s/$albumArtistQuery//") | sed -e "s/[:\/]/_/")
 
         albumFolder="$musicPath/$albumArtist/$album"
 
@@ -38,8 +36,8 @@ tidy(){
             mkdir -p $albumFolder               # Make it. Make it using -p so it also makes the folder for album artist if needs be
             echo "Creating folder for $album"
         fi
-        echo "Moving $mp3File to $albumFolder"
-        mv $mp3File $albumFolder/$(basename $mp3File)   # Move the file from the base directory into the requisite folder
+        echo "Moving $mp3Base to $albumFolder"
+        mv $mp3File $albumFolder/$mp3Base   # Move the file from the base directory into the requisite folder
         echo "Moving successful!"
     fi
 }
@@ -67,3 +65,5 @@ for musicFile in $files; do
 done
 
 IFS=$SAVEIFS
+
+# After writing this I'm not sure if sed's my favourite or least favourite function.
