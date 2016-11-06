86e719050cbe44628d790470ddb61056ce718fb7
Jack Ellis <jackellis1504@me.com>
2016-11-06 12:47:44 +0000

    Got rid of the file count on ls, as it's shit

--- Scripts/BackupMusic.sh
+++ Scripts/BackupMusic.sh
@@ -3,16 +3,18 @@
 SAVEIFS=$IFS            # Filenames with spaces are tricky buggers
 IFS=$(echo -en "\n\b")  # This is how you get around it. Just...
 
-#files=$(find ~/Music/iTunes/iTunes\ Media/Music -name *.m4a)
+#files=$(find /Users/Jack/Music/iTunes/iTunes\ Media/Music -name *.m4a)
 files=~/Desktop/Scratch/*.m4a
-destination="illiac-local:~/Mass/Music"
+destination="illiac-local:/mnt/usbStick/Music"
 
 for m4aFile in $files
 do
-    mp3File=$(echo ${m4aFile} |sed -e 's/m4a/mp3/')             # Make a filename for the mp3 versions
-    mp3Base=$(basename $mp3File)                                # For debugging, get rid of the file path
-    echo "Converting $(basename $m4aFile) to $mp3Base"
-    avconv -i $m4aFile -ab 320k -ac 2 -ar 44100 $mp3File        # Convert from m4a to 320kbps mp3
+    if [ "${m4aFile: -4}" == ".m4a" ]; then
+        mp3File=$(echo ${m4aFile} |sed -e 's/m4a/mp3/')             # Make a filename for the mp3 versions
+        mp3Base=$(basename $mp3File)                                # For debugging, get rid of the file path
+        echo "Converting $(basename $m4aFile) to $mp3Base"
+        avconv -v quiet -i $m4aFile -ab 320k -ac 2 -ar 44100 $mp3File        # Convert from m4a to 320kbps mp3
+    fi
     echo "Copying $mp3Base to $destination"
     scp $mp3File $destination                                   # Copy file to ssh server
     echo "Deleting local copy of $mp3Base"
