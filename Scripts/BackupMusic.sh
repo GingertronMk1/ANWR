a8da1b238466e335a3e93d70210bfb3e6edf8abb
Jack Ellis <jackellis1504@me.com>
2016-11-08 13:47:44 +0000

    ConvertAndTidy used for testing, got rid of some more unnecessary code from BackupMusic

--- Scripts/BackupMusic.sh
+++ Scripts/BackupMusic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SAVEIFS=$IFS            # Filenames with spaces are tricky buggers
-IFS=$(echo -en "\n\b")  # This is how you get around it. Just...
+SAVEIFS=$IFS            # Back up the current IFS character
+IFS=$(echo -en "\n\b")  # Replace it with \n\b so spaces aren't an issue
 
 destServer="$1"         # The destination is specified as an argument
 
@@ -9,37 +9,34 @@ musicFiles=~/Desktop/Scratch/*.m4a
 #destFolder=/mnt/usbStick/Music
 destFolder=/mnt/usbStick/Scratch
 
-queryMetadata() {                                                                                       # Strip query out of metadata, and while you're there,
-    echo $(avprobe "$1" 2>&1 | grep "$2") | sed -e "s/$2//g" | sed -e "s/[:\/\.]/_/g"                   # replace FS-problematic chars with underscores
+queryMetadata() {                                                                       # Strip query out of metadata, and while you're there,
+    echo $(avprobe "$1" 2>&1 | grep "$2") | sed -e "s/$2//g" | sed -e "s/[:\/\.]/_/g"   # replace FS-problematic chars with underscores
 }
 
 for m4aFile in $musicFiles
 do
+    # Preamble
     m4aBase=$(basename $m4aFile)
 
-    # Convert
-    if [ "${m4aFile: -4}" == ".m4a" ]; then                             # If it is an m4a file:
-        mp3File=$(echo "$m4aFile" | sed -e 's/m4a/mp3/')                # Make a filename for the mp3 versions
+    # Convert original from m4a to mp3
+    if [ "${m4aFile: -4}" == ".m4a" ]; then
+        mp3File=$(echo "$m4aFile" | sed -e 's/m4a/mp3/')
         echo "Converting $m4aBase to mp3"
-        avconv -v quiet -i $m4aFile -ab 320k -ac 2 -ar 44100 $mp3File   # Convert m4as to mp3s (320kbps, 2-channel, 44100Hz sample rate)
-    else
-        echo "$m4aBase is already an mp3. Skipping conversion"          # If it's an mp3, don't bother with all that
-        mp3File="$m4aFile"
+        avconv -v quiet -i $m4aFile -ab 320k -ac 2 -ar 44100 $mp3File
     fi
     
-    # Query
-    album=$(queryMetadata "$mp3File" "    album           : ")          # Query avprobe for album name
-    albumArtist=$(queryMetadata "$mp3File" "    album_artist    : ")    # and album artist name, then
-    albumFolder="$destFolder/$albumArtist/$album"                       # make a folder path based on these
-    destination="$destServer:'$albumFolder'"                            # and append that to the destination server
+    # Query avprobe for artist and album artist, and create a file path from these
+    album=$(queryMetadata "$mp3File" "    album           : ")
+    albumArtist=$(queryMetadata "$mp3File" "    album_artist    : ")
+    albumFolder="$destFolder/$albumArtist/$album"
 
-    # Pre-Tidy
-    sshQuery="[ ! -d '$albumFolder' ] && (mkdir -p '$albumFolder'; echo '$albumFolder' not found, creating...)"   # See below
-    ssh $destServer "$sshQuery"     # SSH into the destination, create album folder if it doesn't exist
+    # Pre-tidy by SSHing into the destination and creating a folder for the album based on the above querying
+    sshQuery="[ ! -d '$albumFolder' ] && (mkdir -p '$albumFolder'; echo '$albumFolder' not found, creating...)"
+    ssh $destServer "$sshQuery"
 
-    # Copy
-    scp "$mp3File" "$destination"                                       # Copy mp3 file to server
-    rm $mp3File                                                         # Delete local copy of file
+    # Copy mp3 file to the server, into the album folder created above, then delete the local copy
+    scp "$mp3File" "$destServer:$albumFolder"
+    rm $mp3File
 done
 
-IFS=$SAVEIFS    # ...don't forget to put things back where you found them
+IFS=$SAVEIFS    # Finally, put IFS back to how it was before
