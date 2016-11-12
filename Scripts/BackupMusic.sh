d2fd7229033d8a3806b71110e3cd412b52018690
Jack Ellis <jackellis1504@me.com>
2016-11-12 17:01:39 +0000

    Prompty things again to unbreak it

--- Scripts/BackupMusic.sh
+++ Scripts/BackupMusic.sh
@@ -5,6 +5,7 @@ IFS=$(echo -en "\n\b")  # Replace it with \n\b so spaces aren't an issue
 
 destServer="$1"         # The destination is specified as an argument
 
+#musicFiles="find -name /Users/Jack/Music/iTunes/iTunes Media/Music/*.m4a
 musicFiles=~/Desktop/Scratch/*.m4a
 #destFolder=/mnt/usbStick/Music
 destFolder=/mnt/usbStick/Scratch
@@ -19,16 +20,9 @@ do
     # Preamble
     m4aBase=$(basename $m4aFile)
 
-    # Convert original from m4a to mp3
-    if [ "${m4aFile: -4}" == ".m4a" ]; then
-        mp3File=$(echo "$m4aFile" | sed -e 's/m4a/mp3/')
-        echo "Converting $m4aBase to mp3"
-        avconv -v quiet -i $m4aFile -ab 320k -ac 2 -ar 44100 $mp3File
-    fi
-    
     # Query avprobe for artist and album artist, and create a file path from these
-    album=$(queryMetadata "$mp3File" "    album           : ")
-    albumArtist=$(queryMetadata "$mp3File" "    album_artist    : ")
+    album=$(queryMetadata "$m4aFile" "    album           : ")
+    albumArtist=$(queryMetadata "$m4aFile" "    album_artist    : ")
     albumFolder="$destFolder/$albumArtist/$album"
 
     # Pre-tidy by SSHing into the destination and creating a folder for the album based on the above querying
@@ -36,8 +30,7 @@ do
     ssh $destServer "$sshQuery"
 
     # Copy mp3 file to the server, into the album folder created above, then delete the local copy
-    scp "$mp3File" "$destServer:$albumFolder"
-    rm $mp3File
+    scp "$m4aFile" "$destServer:$albumFolder"
 done
 
 IFS=$SAVEIFS    # Finally, put IFS back to how it was before
