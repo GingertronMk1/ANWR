aefd29dab6a3ae1719adc6447f05d09ded01996c
Jack Ellis <jackellis1504@me.com>
2017-02-02 08:06:39 +0000

    Finally got around to making this work properly again

--- NewSystem.sh
+++ NewSystem.sh
@@ -2,29 +2,21 @@
 
 # Points things in the right direction for usefulness
 
-scriptDir="$(pwd -P)"
-dotDir="$scriptDir/Dotfiles"
-dotFiles=$dotDir/*
+thisDir=$(pwd -P)
 
-#echo Script directory: $scriptDir
-#echo Dotfile directory: $dotDir
-#echo Dotfiles: $dotFiles
-
-cd ~
-
-for file in "$dotFiles"
+for file in `find ./Dotfiles -type f`
 do
-    [ -e $file ] && (rm $file; echo "Deleting existing copy of $file in ~")
-    ln -s $dotDir/$file # Create a symlink to this directory's copy in ~
+  baseFile=$(basename $file)
+  fullFile=$(pwd -P)/Dotfiles/$baseFile
+  echo File: $fullFile
+  [ -e ~/$baseFile ] && (rm ~/$baseFile; echo "Deleting existing copy of $baseFile in ~")
+  ln -s $fullFile ~ # Create a symlink to this directory's copy in ~
+  echo "Created new link to $fullFile in ~"
 done
 
-[ ! -e .vim ] && (mkdir -p .vim; echo "Creating .vim folder")        # If the .vim folder doesn't exist, create it
-[ -e .vim/colors ] && (rm -rf .vim/colors; echo "Deleting .vim/colors folder")  # If the file/folder .vim/colors exists, delete it
-ln -s $scriptDir/vimcolors .vim/colors                      # Symlink to the colors directory in this folder
+[ ! -e ~/.vim ] && (mkdir -p ~/.vim; echo "Creating .vim folder")        # If the .vim folder doesn't exist, create it
+[ -e ~/.vim/colors ] && (rm -rf ~/.vim/colors; echo "Deleting .vim/colors folder")  # If the file/folder .vim/colors exists, delete it
+ln -s $thisDir/vimcolors ~/.vim/colors                      # Symlink to the colors directory in this folder
 echo "Linked .vim/colors to color folder in ANWR"           # Tell me what it's done
 
 touch .hushlogin        # Create a .hushlogin file so the system shuts the fuck up when I log in
-
-. .bashrc
-
-cd -
