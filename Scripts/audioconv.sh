84cdc4af1e915c5d946f970804750a70eb90a6ef
Jack Ellis <jackellis1504@me.com>
2017-10-14 14:10:38 +0100

    Made scratch audio converter threaded as well as getting rid of useless echos

--- Scripts/audioconv.sh
+++ Scripts/audioconv.sh
@@ -2,6 +2,7 @@
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
+set -m
 
 #musicPath=/mnt/usbStick/Music
 folder=$SCRATCHFOLDER   # Where the file things are (that you want copying)
@@ -11,49 +12,46 @@ files=$(find $folder -name "*.flac")
 mp3Convert() {
   baseFile=$1
   newFile=$(echo $line | sed -e 's/flac/mp3/g')
-  echo "Converting $1 to $newFile"
   avconv -v quiet -i $baseFile -ab 320k -ac 2 -ar 44100 $newFile
-  echo "Conversion successful!"
 }
 
 flacConvert() {
   baseFile=$1
   newFile=$(echo $line | sed -e 's/flac/flac/g')
-  echo "Converting $baseFile to $newFile"
   avconv -v quiet -i $baseFile -f flac $newFile
-  echo "Conversion successful!"
 }
 
 wavConvert() {
   baseFile=$1
   newFile=$(echo $line | sed -e 's/flac/wav/g')
-  echo "Converting $baseFile to $newFile"
   avconv -v quiet -i $baseFile -f wav $newFile
-  echo "Conversion successful!"
 }
 
 waveformConvert() {
-  echo "Generating waveform png for $1"
   newFile=$(echo $line | sed -e 's/flac/png/g')
   ffmpeg -v quiet -i $1 -filter_complex "showwavespic=s=6400x1200" -frames:v 1 $newFile
-  echo "Generation successful!"
 }
 
 echo -e "Convert to:\n[1]: mp3\n[2]: flac\n[3]: wav\n[4]: waveform png\n[0]: flac & wav"
 read -n 1 -r
 echo -e "\n"
 
+STARTTIME=$(date +%s)
+
 for line in $files; do
   case $REPLY in
-    1) mp3Convert $line && rm $line;;
-    2) flacConvert $line && rm $line;;
-    3) wavConvert $line && rm $line;;
-    4) waveformConvert $line;;
-    0) flacConvert $line && wavConvert $line;;
+    1) mp3Convert $line && rm $line &;;
+    2) flacConvert $line && rm $line &;;
+    3) wavConvert $line && rm $line &;;
+    4) waveformConvert $line &;;
+    0) flacConvert $line && wavConvert $line &;;
   esac
-  echo -e "Next!\n"
 done
 
 IFS=$SAVEIFS
+
+wait
+
+echo "Done!"
 
 # After writing this I'm not sure if sed's my favourite or least favourite function.
