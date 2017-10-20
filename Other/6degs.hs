348a672e08d78b861f40d94fc6aa51c51b770f88
Jack Ellis <jackellis1504@gmail.com>
2017-10-20 11:27:44 +0100

    Updated with better filtering (dotfiles and names), and a new treeGen approach

--- Other/6degs.hs
+++ Other/6degs.hs
@@ -5,50 +5,43 @@ import Data.Char
 
 data ActorTree a = Node a [ActorTree a] deriving Show
 
-treeGen :: String -> String -> [String] -> ActorTree String
-treeGen actorTarget actorBase actorsDone = if actorTarget == actorBase || elem actorBase actorsDone then Node actorBase []
-                                                                                                    else Node actorBase [treeGen actorTarget b (actorBase:actorsDone) | b <- allFellow actorBase]
+showsPath :: String
+showsPath = "/Users/Jack/Git/history-project/_shows/"
+
+me :: String
+me = "Jack Ellis"
 
 limitTree :: Int -> ActorTree String -> ActorTree String
 limitTree 0 (Node x _) = Node x []
 limitTree n (Node x ts) = Node x [limitTree (n-1) t | t <- ts]
 
-sixDegrees'' actorTarget actorBase = limitTree 4 $ treeGen actorTarget actorBase []
+treeGen' actorName actorsDone = if elem actorName actorsDone then Node actorName []
+                                                             else Node actorName [treeGen' a (actorName:actorsDone) | a <- allFellow actorName, notElem a actorsDone]
 
---sixDegrees :: String -> Int -> ActorTree String -> Int
-sixDegrees' actorTarget n (Node p []) = 99
-sixDegrees' actorTarget n (Node p ps) = if p == actorTarget then n
-                                                            else minimum $ map (sixDegrees' actorTarget (n+1)) ps
+treeGen actorName = treeGen' actorName []
 
-sixDegrees actorTarget actorBase = sixDegrees' actorTarget 1 (sixDegrees'' actorTarget actorBase)
+limitedTree actorName = limitTree 5 $ treeGen actorName
 
-sixDegsTest = sixDegrees "Jamie Drew" "Ian Sheard"
 
-showsPath :: String
-showsPath = "/Users/Jack/Git/history-project/_shows/"
-
---names :: String -> IO [String]
 names s = do filecontent <- readFile s
              let peopleNames = map (drop 2) $ map (dropWhile (/= ':')) $ filterPeople $ lines filecontent
                                where filterPeople ss = filter peopleFilter ss
-                                     peopleFilter s = (take 5 (dropWhile (/= 'n') s)) == "name:"
-                               --where filterPeople ss = filter peopleFilter ss
-                                     --peopleFilter s = (take 10 s) == "    name: "
+                                     peopleFilter s = isInfixOf " name:" s
              return peopleNames
 
-
-
+getAllShows :: IO [FilePath]
 getAllShows = do showsDirs <- getDirectoryContents showsPath
                  return showsDirs
 
-allShowsEver  = flatten $ allShowsBuilder getAllShows
+allShowsEver :: [String]
+allShowsEver = flatten $ allShowsBuilder getAllShows
 
 showsPlusPathFun :: [[Char]] -> [[Char]]
 showsPlusPathFun s = map (showsPath++) s
 
 filterDots :: [[Char]] -> [[Char]]
 filterDots l = filter dotsFilter l
-               where dotsFilter f = (f /= "." && f /= "..")
+               where dotsFilter f = head f /= '.'
 
 getAllContents :: [FilePath] -> [IO [FilePath]]
 getAllContents s = map getDirectoryContents s
@@ -74,7 +67,12 @@ namess = filter (/= []) $ map unsafePerformIO $ map names $ drop 102 allShowsEve
 rmdups :: Ord a => [a] -> [a]
 rmdups = map head . group . sort
 
+allFellow :: String -> [String]
 allFellow n = filter (/= n) $ rmdups [a | as <- allFellow' n, a <- as]
               where allFellow' n = filter (elem n) namess
 
+flatten :: [[a]] -> [a]
 flatten ass = [a | as <- ass, a <- as]
+
+everyone :: [String]
+everyone = rmdups $ flatten $ namess
