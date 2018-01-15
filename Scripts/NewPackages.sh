7a9fbba89dab71b0e882f157aec5663a5bb55735
Jack Ellis <jackellis1504@gmail.com>
2018-01-15 20:08:38 +0000

    Cleaning after moves to new Eldorado repo

--- Scripts/NewPackages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install chromium-browser vim haskell-platform htop xinput libav-tools samba -y
--- Scripts/weeklyupdate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
